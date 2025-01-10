eee7a4b7489965b916cedda1c048e0512bddb59e
sandratur <sandra.turesson.webd24jon@edu.tucsweden.se>
2025-01-10 20:12:24 +0100

    Testat

--- TodoAppWPF/MainWindow.xaml.cs
+++ TodoAppWPF/MainWindow.xaml.cs
@@ -16,19 +16,46 @@ namespace TodoAppWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TodoList _todolist;
         public MainWindow()
         {
             InitializeComponent();
+            _todolist = new TodoList();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">sender fr mainwindow</param>
+        /// <param name="e">event</param>
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todolist.AddTask(task);
+                UpdateTaskList();
+                TaskTextBox.Clear();
+            }
+
+        }
+
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todolist.GetAllTasks())
+            {
+                TasksListBox.Items.Add(task);
+            }
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _todolist.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
+            }
         }
     }
 }
