4236337955265ca16c663ff958b82574691dd866
sandratur <sandra.turesson.webd24jon@edu.tucsweden.se>
2025-01-10 10:18:40 +0100

    Class ToDoLIst - grunden

---
+++ TodoAppWPF/TodoList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppWPF
+{
+    //Hanterar logiken för att lägga till, ta bord samt lägga till xml filer
+    public class TodoList
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+            
+        }
+
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
+    }
+}
